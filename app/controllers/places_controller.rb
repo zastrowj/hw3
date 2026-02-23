4034a8b42fdea84b3e96276f815eb23a2257753a
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-02-23 21:00:29 +0000

    add missing end statement to places_controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,20 +3,21 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
-def show
-  @place = Place.find_by({ "id" => params["id"] })
-  @entries = Entry.where({ "place_id" => @place["id"] })
-end
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"] })
+  end
 
   def new
     @place = Place.new
   end
 
-def create
-  @place = Place.new
-  # Change this to use the simple params syntax from your template
-  @place["name"] = params["name"]
-  @place.save
-  redirect_to "/places"
-end
+  def create
+    @place = Place.new
+    # Change this to use the simple params syntax from your template
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
 
+end
