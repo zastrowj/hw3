d4ca8455eb2b967466df8d1cb2bccac3c2ee3a56
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-02-23 20:58:43 +0000

    resolve missing parameter error for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,25 +12,11 @@ end
     @place = Place.new
   end
 
-  def create
-    @place = Place.new(place_params)
-    if @place.save
-      redirect_to places_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def place_params
-    params.require(:place).permit(:name)
-  end
-end
-
 def create
   @place = Place.new
-  @place["name"] = params["name"] # This "name" matches the 'name' attribute in your form input
+  # Change this to use the simple params syntax from your template
+  @place["name"] = params["name"]
   @place.save
-  redirect_to "/places" # Sends the user back to the list after saving
+  redirect_to "/places"
 end
+
