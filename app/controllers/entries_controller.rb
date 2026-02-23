b8f8847bafbbb1a97561b9de81c29620852f6068
Jacob Zastrow <252672008+zastrowj@users.noreply.github.com>
2026-02-23 20:44:53 +0000

    enable managing of journal entries for new data and how it's saved

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,22 +1,26 @@
 class EntriesController < ApplicationController
+
   def new
-    @place = Place.find(params[:place_id]) if params[:place_id]
-    @entry = Entry.new(place_id: @place&.id)
+    # We find the place first so the "New Entry" page can show 
+    # which city we are adding a note for.
+    @place = Place.find_by({ "id" => params["place_id"] })
   end
 
   def create
-    @entry = Entry.new(entry_params)
-    if @entry.save
-      redirect_to place_path(@entry.place_id)
-    else
-      @place = Place.find(@entry.place_id)
-      render :new
-    end
+    # 1. Create a new blank Entry object
+    @entry = Entry.new
+
+    # 2. Fill the columns with data from the form (params)
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+    @entry["place_id"] = params["place_id"]
+
+    # 3. Save the row to the database
+    @entry.save
+
+    # 4. Redirect back to the specific place's show page
+    redirect_to "/places/#{@entry["place_id"]}"
   end
 
-  private
-
-  def entry_params
-    params.require(:entry).permit(:title, :description, :posted_on, :place_id)
-  end
 end
